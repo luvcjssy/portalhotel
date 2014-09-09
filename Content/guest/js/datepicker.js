db10ed6b1d86a6560b98a89d162f5cebe48e94bb
Hoan Nguyen <nguyenhoan2510@gmail.com>
2014-09-10 00:30:40 +0700

    cm

---
+++ Content/guest/js/datepicker.js
@@ -0,0 +1,18 @@
+﻿
+    $(function () {
+        $("#datepicker").datepicker({
+            dateFormat: "dd/mm/yy",
+            showOn: "both",
+            buttonImage: "http://www.khachsan247.com/templates/Hotel/images/icon_calendar.jpg",
+            buttonImageOnly: true,
+        });
+        $("#datepicker2").datepicker({
+            dateFormat: "dd/mm/yy",
+            showOn: "both",
+            buttonImage: "http://www.khachsan247.com/templates/Hotel/images/icon_calendar.jpg",
+            buttonImageOnly: true,
+        });
+
+        $('#datepicker').datepicker().datepicker('setDate', new Date());
+        $('#datepicker2').datepicker().datepicker('setDate', new Date());
+    });
